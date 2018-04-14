a9cfb11fae8944eef3dacb1013bf2deb633e23f7
Chris Knight <cknight@casenetllc.com>
2018-04-13 19:52:08 -0700

    added chrome no sandbox

--- ng-form-rules/karma.conf.js
+++ ng-form-rules/karma.conf.js
@@ -27,7 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless', 'Chrome'],
+    browsers: ['ChromeHeadless', 'Chrome', "ChromeNoSandbox"],
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
